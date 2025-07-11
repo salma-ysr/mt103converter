94fb461a0db6a04b65e4cb0102875d114d437816
Salma Yousry <salmayousry@Salmas-MacBook-Pro.local>
2025-07-11 12:56:17 -0400

    first

---
+++ src/main/java/com/attijari/MT103converter/Mt103converterApplication.java
@@ -0,0 +1,13 @@
+package com.attijari.MT103converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mt103converterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mt103converterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/attijari/MT103converter/Mt103converterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attijari.MT103converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mt103converterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
